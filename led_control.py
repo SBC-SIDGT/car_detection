72cf63da01ee4690c7b2a812c77592b1fa04f6dc
Javinator9889 <javialonso007@hotmail.es>
2019-12-18 17:56:26 +0100

    Ok

--- led_control.py
+++ led_control.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python
-
-import sys
-import mraa
-import time
-import os
-
+# #!/usr/bin/python
+#
+# import sys
+# import mraa
+# import time
+# import os
 
 
 def led_control(led_status):
@@ -15,14 +14,11 @@ def led_control(led_status):
     *    The second char control the green yellow, 0 Off 1 On
     *    The third char control the green red, 0 Off 1 On
     """
-    with open("/sys/class/leds/upboard:green:/brightness", 'w') as green
-    , open("/sys/class/leds/upboard:yellow:/brightness", 'w') as yellow
-    , open("/sys/class/leds/upboard:red:/brightness","w") as red:
-    
+    with open("/sys/class/leds/upboard:green:/brightness", 'w') as green, \
+            open("/sys/class/leds/upboard:yellow:/brightness", 'w') as yellow, \
+            open("/sys/class/leds/upboard:red:/brightness", 'w') as red:
         assert len(led_status) == 3
 
-        green.write(led_status[0])   
+        green.write(led_status[0])
         yellow.write(led_status[1])
         red.write(led_status[2])
-
-
--- road_mask.py
+++ road_mask.py
@@ -11,6 +11,8 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 
+from led_control import led_control
+
 
 class CarDetector:
     def __init__(self, xml_file: str = "cars.xml"):
@@ -20,9 +22,9 @@ class CarDetector:
     def _masked_image(image, polygons, show_result=False):
         mask = sum([polygon2mask(image.shape[:2], polygon)
                     for polygon in polygons])
-        img[:, :, 0] = img[:, :, 0] * mask
-        img[:, :, 1] = img[:, :, 1] * mask
-        img[:, :, 2] = img[:, :, 2] * mask
+        image[:, :, 0] = image[:, :, 0] * mask
+        image[:, :, 1] = image[:, :, 1] * mask
+        image[:, :, 2] = image[:, :, 2] * mask
 
         if show_result is True:
             plt.figure(figsize=(40, 40))
@@ -30,7 +32,7 @@ class CarDetector:
             plt.title('masked image result')
             plt.show()
 
-        return img
+        return image
 
     def _detection(self, img, show_result=False):
         cars = self._cascade.detectMultiScale(img, 1.1, 1)
@@ -74,44 +76,39 @@ class CarDetector:
                 for _, polygon in enumerate(polygons)]
 
 
-# def main(url=None, debug=False):
-#     # if url is not None:
-#     # url = input("Remote server URL: ")
-#
-#     try:
-#         st = time()
-#         frames = 0
-#         while True:
-#             # opener = urlopen(url)
-#             npimage = cv2.imdecode(np.frombuffer(opener.read(), np.uint8), -1)
-#             car_detector(npimage)
-#             if debug is True:
-#                 frames += 1
-#                 endt = time()
-#                 if endt - st >= 1:
-#                     print("              ", end="\r")
-#                     print(f"FPS: {frames}", end="\r")
-#                     frames = 0
-#                     st = endt
-#     except KeyboardInterrupt:
-#         exit(0)
-#
-#
-# app = Flask(__name__)
-#
-#
-# @app.route('/', methods=["POST"])
-# def receive_image():
-#     nparr = np.frombuffer(request.data, np.uint8)
-#     image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-#     return jsonify({"status": 200}), 200
-
-img = "885.jpg"
-image = cv2.imread(img)
+app = Flask(__name__)
 detector = CarDetector()
-rs = detector.detect(image, show_result=False)
-for i in rs:
-    print(len(i))
-print(rs)
+
+
+@app.route('/', methods=["POST"])
+def receive_image():
+    nparr = np.frombuffer(request.data, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    vehicles = [len(x) for x in detector.detect(image)]
+    return jsonify({"vehicles": vehicles}), 200
+
+
+@app.route("/data", methods=["POST"])
+def receive_semaphore_status():
+    status = request.get_json()
+    if status is None:
+        return jsonify({"status": "error"}), 400
+    mode = status.get("mode")
+    if mode is None:
+        return "No mode", 400
+    led_control("{0:b}".format(mode))
+    return "Ok", 200
+
+
+app.run(port=5000)
+
+
+# pimg = "885.jpg"
+# oimage = cv2.imread(pimg)
+# detector = CarDetector()
+# rs = detector.detect(oimage, show_result=False)
+# for i in rs:
+#     print(len(i))
+# print(rs)
 
 # main()
